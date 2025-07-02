c130fe2a9be2eb207e7dd69a1e2971bcd6dbdf63
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-07-02 23:03:52 +0530

    Add Spline integration and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-
+import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <div>
+      <div>
+      <Spline scene="https://prod.spline.design/i0W2W9FWUxNNuEHv/scene.splinecode"/>
+      </div>
       <h1 className="font-bold text-5xl">Home</h1>
       <div className="flex items-center justify-between my-7">
         <h1 className="text-2xl">Snippets</h1>
